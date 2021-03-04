8a23d52c0a2292dd80418f7dbbac9b799c0b65c3
Tifany Rosas <rosastifany@gmail.com>
2021-03-04 19:38:09 -0300

    Add user validations

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -8,6 +8,18 @@ module.exports.createUser = async (req, res) => {
 	const username = req.body.username;
 	const password = req.body.password;
 
+	const validationParams = userService.validateUsernameAndPassword(req.body);
+	if (validationParams.error) {
+		res.status(400).send({message: "Failed! Username or password is incorrect!"});
+		return;
+	}
+
+	const duplicateUsers = await userService.validateDuplicateUsers(username);
+	if (duplicateUsers) {
+		res.status(400).send({message: "Failed! Username or password is incorrect!"});
+		return;
+	}
+
 	try {
 		const newUser = await userService.createUser(username, password);
 		res.status(201).json({id: newUser.id});
--- services/user.service.js
+++ services/user.service.js
@@ -1,13 +1,40 @@
+const bcryptjs = require('bcryptjs');
+const Joi = require('joi');
 const db = require("../models");
+
+const validateDuplicateUsers = async (username) => {
+  return await db.user.findOne({ where: { username } });
+}
+
+const validateUsernameAndPassword = (req) => {
+
+  const schema = Joi.object({ 
+    username: Joi.string() 
+          .min(3) 
+          .max(30) 
+          .required(), 
+    password: Joi.string() 
+          .min(5) 
+          .max(10)
+          .required()
+  }).options({ abortEarly: false }); 
+
+  return schema.validate(req) 
+}
 
 const createUser = async (username, password) => {
 
+  const salt = await bcryptjs.genSaltSync(10);
+  const password_hash = await bcryptjs.hashSync(password, salt);
+
   return await db.user.create({
     username: username,
-    password: password
+    password: password_hash
   });
 }
 
 module.exports = {
+  validateDuplicateUsers,
+  validateUsernameAndPassword,
   createUser
 }
