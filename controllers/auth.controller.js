a7f9684345e007962bc957c4c3fd8b4a9b471285
Tifany Rosas <rosastifany@gmail.com>
2021-03-04 19:46:26 -0300

    Add login functionality

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,40 @@
-const login = require('../models/login');
+const jwt = require('jsonwebtoken');
+const bcryptjs = require('bcryptjs');
+const secretKey = require("../config/auth.config.js");
+const db = require("../models");
 
 /**
  * Login allows the user to authenticate with credentials 
  * and get a token to use on secured endpoints. 
  */
-module.exports.login = (req, res) => {
-  // TODO: User must login and a token must be generated
-  res.status(200).send(login);
+module.exports.login = async (req, res) => {
+  	const username = req.body.username;
+	const password = req.body.password;
+
+	if (!username || !password) {
+		res.status(400).send({message: "Failed! Missing required parameters"});
+		return;
+	}
+
+  	const user = await db.user.findOne({ where: { username } });
+  	if (user) {
+  		const equalPasswords = bcryptjs.compareSync(password, user.password);
+        if (equalPasswords) {
+			res.status(200).json({
+				id: user.id,
+				token: generateToken(user.id)
+			});
+		} else {
+			res.status(400).send({message: "Failed! Username or password is incorrect"});
+		}
+	} else {
+		res.status(400).send({message: "Failed! Username or password is incorrect"});
+	}
 };
+
+function generateToken(userId) {
+	const payload = {
+		id: userId
+	}
+	return jwt.sign(payload, secretKey, { expiresIn: 1440 });
+}
